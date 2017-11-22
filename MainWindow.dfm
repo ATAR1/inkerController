af2fb84a33979cc2e9da245d36b57d2949462b48
ATAR1 <thexenosmilu@gmail.com>
2017-11-22 15:38:19 +0500

    2

--- MainWindow.dfm
+++ MainWindow.dfm
@@ -1,7 +1,7 @@
 object Form1: TForm1
   Left = 0
   Top = 0
-  Caption = #1059#1087#1088#1072#1074#1076#1077#1085#1080#1077' '#1082#1088#1072#1089#1082#1086#1086#1090#1084#1077#1090#1095#1080#1082#1086#1084
+  Caption = #1059#1087#1088#1072#1074#1083#1077#1085#1080#1077' '#1082#1088#1072#1089#1082#1086#1086#1090#1084#1077#1090#1095#1080#1082#1086#1084
   ClientHeight = 299
   ClientWidth = 635
   Color = clBtnFace
@@ -15,9 +15,55 @@ object Form1: TForm1
   OnDestroy = FormDestroy
   PixelsPerInch = 96
   TextHeight = 13
+  object Indicator1: TIndicator
+    Left = 40
+    Top = 24
+    Width = 17
+    Height = 25
+    Brush.Color = clGray
+    Shape = stCircle
+  end
+  object Indicator2: TIndicator
+    Left = 63
+    Top = 24
+    Width = 17
+    Height = 25
+    Brush.Color = clGray
+    Shape = stCircle
+  end
+  object Indicator3: TIndicator
+    Left = 86
+    Top = 24
+    Width = 17
+    Height = 25
+    Brush.Color = clGray
+    Shape = stCircle
+  end
+  object Indicator4: TIndicator
+    Left = 109
+    Top = 25
+    Width = 17
+    Height = 25
+    Brush.Color = clGray
+    Shape = stCircle
+  end
   object Label1: TLabel
-    Left = 120
-    Top = 56
+    Left = 64
+    Top = 104
+    Width = 39
+    Height = 13
+    Caption = 'Label1'
+  end
+  object Label2: TLabel
+    Left = 64
+    Top = 123
+    Width = 31
+    Height = 13
+    Caption = 'Label1'
+  end
+  object Label3: TLabel
+    Left = 64
+    Top = 142
     Width = 31
     Height = 13
     Caption = 'Label1'
--- MainWindow.pas
+++ MainWindow.pas
@@ -8,15 +8,20 @@ uses
 
 type
   TForm1 = class(TForm)
+    Indicator1: TIndicator;
+    Indicator2: TIndicator;
+    Indicator3: TIndicator;
+    Indicator4: TIndicator;
     Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
-    procedure Shape1ContextPopup(Sender: TObject; MousePos: TPoint;
-      var Handled: Boolean);
 
   private
     mainCycleTimer:TTimer;
     procedure Cycle(Sender: TObject);
+    procedure ShowIndicators(inputByte: Word);
   public
     { Public declarations }
   end;
@@ -27,8 +32,51 @@ var
 implementation
 uses FakeDriver;
 {$R *.dfm}
-var speed:integer;
+var speed,speed2:real;
     baseAdress:word;
+    storedState:byte;
+    onTime:array [0..3] of longint;
+    offTime:array [0..3] of longint;
+    distance:array [0..2] of integer;
+
+function GetSpeed(time1:longint;time2:longint;intervalNum:integer):real;
+begin
+  if((time1<0) or (time2<0) or (time1=time2) ) then exit(-1);
+  Result:=distance[intervalNum]/(time1-time2);
+end;
+
+procedure SetDetector(detectorNum:integer; state:byte);
+begin
+  if(state>0) then
+    begin
+      onTime[detectorNum]:= GetTickCount();
+      if(detectorNum>0)then
+        speed:=GetSpeed(onTime[detectorNum],onTime[detectorNum-1],detectorNum-1);
+      storedState:=storedState or (1 shl detectorNum);
+    end
+  else
+    begin
+      offTime[detectorNum]:=GetTickCount();
+      if(detectorNum>0)then
+        speed2:=GetSpeed(offTime[detectorNum],offTime[detectorNum-1], detectorNum-1);
+      storedState:=storedState and not (1 shl detectorNum);
+    end;
+
+end;
+
+
+procedure MeasuringSpeed(inputByte:word);
+var i:integer;
+begin
+  for i := 0 to 3 do
+    if(((storedState xor inputByte) and (1 shl i))>0) then
+      begin
+        SetDetector(i,(inputByte and (1 shl i)));
+      end;
+end;
+
+
+
 { TForm1 }
 
 procedure TForm1.Cycle(Sender: TObject);
@@ -36,12 +84,31 @@ var
   inputByte: Word;
 begin
   inputByte:=ISO_InputByte(baseAdress + 0);
-  Label1.Caption := IntToHex(inputByte,2);
-  if(inputByte and 1) <> 0    then
-    Indicator1.SetOn()
-  else
-    Indicator1.SetOff();
+  ShowIndicators(inputByte);
+  MeasuringSpeed(inputByte);
+  Label1.Caption:='—корость по переднему концу = '+IntToStr(Round(speed*1000));
+  Label2.Caption:='—корость по заднему концу = '+IntToStr(Round(speed2*1000));
+  Label3.Caption:='—редн€€ скорость = '+IntToStr(Round((speed+speed2)*1000/2));
+end;
 
+procedure TForm1.ShowIndicators(inputByte: Word);
+begin
+  if (inputByte and 1) <> 0 then
+    Indicator1.SetOn
+  else
+    Indicator1.SetOff;
+  if (inputByte and 2) <> 0 then
+    Indicator2.SetOn
+  else
+    Indicator2.SetOff;
+  if (inputByte and 4) <> 0 then
+    Indicator3.SetOn
+  else
+    Indicator3.SetOff;
+  if (inputByte and 8) <> 0 then
+    Indicator4.SetOn
+  else
+    Indicator4.SetOff;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
@@ -67,7 +134,14 @@ begin
   ISO_DriverClose();
 end;
 
+var i:integer;
+initialization
+  for i := 0 to 2 do
+    distance[i]:=100;
 
-
-
+  for i := 0 to 3 do
+    begin
+      onTime[i]:=-1;
+      offTime[i]:=-1;
+    end;
 end.
